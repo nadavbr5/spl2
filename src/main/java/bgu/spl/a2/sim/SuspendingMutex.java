dcd1679354c78740b7776121e9fcb4e7edcd020b
nadav <nadav>
2017-12-11 14:03:34 +0200

    started part2

--- src/main/java/bgu/spl/a2/sim/SuspendingMutex.java
+++ src/main/java/bgu/spl/a2/sim/SuspendingMutex.java
@@ -1,8 +1,6 @@
 package bgu.spl.a2.sim;
 import bgu.spl.a2.Promise;
 
-import java.util.concurrent.atomic.AtomicBoolean;
-
 /**
  * 
  * this class is related to {@link Computer}
@@ -13,32 +11,28 @@ import java.util.concurrent.atomic.AtomicBoolean;
  *
  */
 public class SuspendingMutex {
-	private AtomicBoolean isLocked=new AtomicBoolean();
 	
-	
+	/**
+	 * Constructor
+	 * @param computer
+	 */
+	public SuspendingMutex(Computer computer){
+		//TODO: replace method body with real implementation
+		throw new UnsupportedOperationException("Not Implemented Yet.");
+	}
 	/**
 	 * Computer acquisition procedure
 	 * Note that this procedure is non-blocking and should return immediatly
 	 * 
-	 * @param computerType
-	 * 					computer's type
-	 * 
 	 * @return a promise for the requested computer
 	 */
 	public Promise<Computer> down(){
-		if (isLocked.compareAndSet(false, true)) {
-
-		} else {
-
-		}
 		//TODO: replace method body with real implementation
 		throw new UnsupportedOperationException("Not Implemented Yet.");
 	}
 	/**
 	 * Computer return procedure
 	 * releases a computer which becomes available in the warehouse upon completion
-	 * 
-	 * @param computer
 	 */
 	public void up(){
 		//TODO: replace method body with real implementation
